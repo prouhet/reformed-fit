2fd19eb221afd6aace109d69c200d5664de12f6e
prouhet <prouhet@gmail.com>
2025-10-19 12:25:51 -0500

    Add initial React setup in index.js
    Initial setup for the React application with logging.

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// This is just setup code for your GitHub repo
+// The actual rendering will happen in your deployed app
+
+const rootElement = document.getElementById('root');
+
+// Your App component will be loaded here after upload
+console.log('App starting...');
