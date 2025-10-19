2cc9b9899476e32c51d3639af3ee629437b6c87f
prouhet <prouhet@gmail.com>
2025-10-19 13:24:18 -0500

    Import supabaseClient for testing in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Force load supabaseClient to test
+import './supabaseClient.js'
+
+console.log('=== MAIN.JSX LOADED ===')
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
